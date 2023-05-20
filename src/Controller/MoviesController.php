83509e85c5a31ccd0b051514cca599a2e4704a77
HsuMonA <HsuMonA@users.noreply.github.com>
2023-05-20 18:06:47 +0200

    how to compile assets

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -12,24 +12,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class MoviesController extends AbstractController
 {
     private $em;
-    public function __construct(EntityManagerInterface $em) {
+    public function __construct(EntityManagerInterface $em)
+    {
         $this->em = $em;
     }
 
     #[Route('/movies', name: 'movies')]
     public function index(): Response
-    {   
+    {
         // findAll() - SELECT * FROM movies;
         // find() - SELECT * from movies WHERE id = 5;
         // findBy() - SELECT * from movies ORDER BY id DESC;
         // findOneBy() - SELECT * from movies WHERE id = 6 AND title = 'The Dark Knight' ORDER BY id DESC;
         // count() - SELECT COUNT() from movies WHERE id = 1;
-        
-        $repository = $this->em->getRepository(Movie::class);
-        $movies = $repository->getClassName();
-
-        dd($movies);
-
         return $this->render('index.html.twig');
     }
 
