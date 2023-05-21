58e48df06dbb007254f5b9d213fbddf6e943a1ad
HsuMonA <HsuMonA@users.noreply.github.com>
2023-05-21 18:08:41 +0200

    how to read blog items

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,30 +2,42 @@
 
 namespace App\Controller;
 
-use App\Entity\Movie;
 use App\Repository\MovieRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    private $em;
-    public function __construct(EntityManagerInterface $em)
+    private $movieRepository;
+    public function __construct(MovieRepository $movieRepository)
     {
-        $this->em = $em;
+        $this->movieRepository = $movieRepository;
     }
 
-    #[Route('/movies', name: 'movies')]
+    #[Route('/movies', methods: ['GET'], name: 'movies')]
     public function index(): Response
     {
-        // findAll() - SELECT * FROM movies;
-        // find() - SELECT * from movies WHERE id = 5;
-        // findBy() - SELECT * from movies ORDER BY id DESC;
-        // findOneBy() - SELECT * from movies WHERE id = 6 AND title = 'The Dark Knight' ORDER BY id DESC;
-        // count() - SELECT COUNT() from movies WHERE id = 1;
-        return $this->render('index.html.twig');
+        //Sql
+        //findAll() - SELECT * FROM movies;
+        //find() - SELECT * from movies WHERE id = 1;
+        //findBy() - SELECT * FROM movies ORDER BY id DESC;
+        //findBy() - SELECT * from movies WHERE id = 1 AND title = 'The Dark Knight'
+        //count() - SELECT COUNT(id) FROM movies
+        $movies = $this->movieRepository->findAll();
+
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies
+        ]);
     }
 
+    #[Route('/movies/{id}', methods: ['GET'], name: 'movie')]
+    public function show($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+
+        return $this->render('movies/show.html.twig', [
+            'movie' => $movie
+        ]);
+    }
 }
